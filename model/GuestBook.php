d60a432d3d9b149c626b2d50b4e8039135855fab
Ortega <ortega.victor@gmail.com>
2020-09-29 16:42:08 +0200

    Add main functionalities

---
+++ model/GuestBook.php
@@ -0,0 +1,53 @@
+<?php
+
+class GuestBook{
+    private $post;
+    private $messages=[];
+    const FILE_NAME = 'model/data/posts.json';
+    
+    public function __construct(){
+        $this->loadPosts();
+    }
+ 
+    public function loadPosts(){
+    //Loading file
+    $myJson=file_get_contents(self::FILE_NAME);
+    $this->messages=unserialize(json_decode($myJson));    
+    }
+
+    public function savePost($post){
+   //Saving the posts
+   array_push($this->messages,$post);
+   $myJson=json_encode(serialize($this->messages));
+   file_put_contents(self::FILE_NAME,$myJson);         
+
+    }
+    /**
+     * Get the value of messages
+     */ 
+    public function getMessages()
+    {
+        $messagesToShow="";
+        $messageOrdered=array_reverse($this->messages);
+      foreach ($messageOrdered as $index => $oneMessage) {
+         if ($index < 20)
+         {  
+            $title=$oneMessage->getTitle();
+            $content=$oneMessage->getContent();
+            $author=$oneMessage->getAuthor()->getName();
+            $email=$oneMessage->getAuthor()->getEmail();
+          
+            $messagesToShow=$messagesToShow."<div class='card text-white bg-info mb-3' >
+            <div class='card-header'>$title</div>
+            <div class='card-body'>
+                <h4 class='card-title'>$author</h4>
+                <p class='card-text'>$content</p>
+            </div>
+            </div>";
+         }
+      }
+
+      return $messagesToShow ;        
+    }
+}
+?>
