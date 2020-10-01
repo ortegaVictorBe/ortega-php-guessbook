4e0da5ef23cb52b5f9101a9634597a7714550b1a
Ortega <ortega.victor@gmail.com>
2020-10-01 23:54:58 +0200

    Modern php app

--- index.php
+++ index.php
@@ -17,59 +17,27 @@ if ($_SERVER["REQUEST_METHOD"]=="POST" ){
     $errMessage="";
     //validate author
     $name=validateAuthor($errMessage);
-
-    // if (empty($_POST["name"]))
-    // {  $errMessage=$errMessage.", Author is empty";
-    // }else{
-    // $name=cleanInput($_POST["name"]); 
-    // }
-
     //Validate e-mail 
     $email=validateEmail($errMessage);
-    // if (empty($_POST["email"])){
-    //     $errMessage=$errMessage."e-mail is empty !";
-    // }else{
-    //  $email=cleanInput($_POST["email"]);
-    //  if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-    //     $errMessage=$errMessage."Invalidate email !";
-    //  } 
-    // }
-
     //Validating title
     $title=validateTitle($errMessage);
-//     if (empty($_POST["title"])){
-//         $errMessage=$errMessage.", Title is empty";
-//    }else{
-//     $title=cleanInput($_POST["title"]); 
-//    }
-    
    //Validate content
    $content=validateContent($errMessage);
-//    if (empty($_POST["content"])){
-//     $errMessage=$errMessage.", Content is empty";
-
-//     }else{
-
-//      $content=cleanInput($_POST["content"]); 
-//     }
 
     if (!empty($errMessage)){
         $errMessage="Please check and fix this issues :) : ".$errMessage;
         $errMessage= " <div class='alert alert-dismissible alert-danger'>     
         <h4 class='alert-heading'>Warning!</h4> 
         <p class='mb-0'>$errMessage
-        </p> </div>";
-   
+        </p> </div>";   
         $userMessage=$errMessage;
-    //    return false; 
+    
       } else{
-        // return true;
+    
         $author=new Visitor($name,$email);
         $newPost= new Message($author,$title,$_POST["content"]);
         $guestBook->savePost($newPost); 
-      }
-
-    
+      }    
 }
 
 //function CleanInput -  Validate the input characters
